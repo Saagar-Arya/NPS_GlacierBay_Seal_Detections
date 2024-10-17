4cf220de077515c1127022dac8e89d659f4e5f54
Saagar Arya <sa553@duke.edu>
2024-10-17 11:14:53 -0400

    saving

--- Full_System_Run.py
+++ Full_System_Run.py
@@ -7,15 +7,15 @@ import time
 def run_scripts():
     start_time = time.time()
 
-    combined_model_path = "Models/CombinedV9.pt"
+    combined_model_path = "Models/CombinedV11.pt"
     color_model_path = "Models/ColorV9.pt"
     inverse_model_path = "Models/InverseV9.pt"
     
-    image_directory = "Sample_Images/"
+    image_directory = r"C:\Users\sa553\Desktop\NPS\JHI_FullSurvey_75m_Survey1 Flight 01"
     
     # run seal detections
     confidence = .85
-    log_file_path = Workflow.run_workflow(combined_model_path, color_model_path, inverse_model_path, image_directory,confidence)
+    log_file_path = Workflow.run_workflow(combined_model_path, combined_model_path, combined_model_path, image_directory,confidence)
 
     # draw seals on photos
     LOG.run_draw_log(log_file_path, image_directory)
